2dc3749bad2786d9659d34fad407db31f31b1356
Emmett Bicker <88175920+EmmettBicker@users.noreply.github.com>
2023-05-25 11:54:53 -0400

    Creating shop

---
+++ src/main/java/game/shop/ShopDoor.java
@@ -0,0 +1,16 @@
+package game.shop;
+
+import java.awt.Point;
+import java.awt.Rectangle;
+
+import game.GameStates.GameplayStates;
+import game.generalSprites.GeneralDoor;
+
+public class ShopDoor extends GeneralDoor{
+
+    public ShopDoor(Point pPlayerEndPos, Rectangle pHitbox) {
+        super(null, pPlayerEndPos, pHitbox);
+        //TODO Auto-generated constructor stub
+    }
+    
+}
